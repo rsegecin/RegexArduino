18b18bf50f9968f1b068ce339fbc49b4aee1bf1a
segecin <rsegecin@hotmail.com>
2017-01-28 19:16:54 -0200

    Code Formatting

--- Interpreter.h
+++ Interpreter.h
@@ -7,26 +7,24 @@
 template<class T, void(T::*PTR)(const char *, const unsigned int, const MatchState &), size_t I>
 struct bind_regex_member
 {
-    typedef void(*fn_type)(const char *, const unsigned int, const MatchState &);
-    explicit bind_regex_member(T* _ptr)
-    {
-        ptr = _ptr;
-    }
-    static void func(const char * match, const unsigned int length, const MatchState & ms)
-    {
-        (ptr->*PTR)(match, length, ms);
-    }
-    operator fn_type()
-    {
-        return &func;
-    }
+	typedef void(*fn_type)(const char *, const unsigned int, const MatchState &);
+	explicit bind_regex_member(T* _ptr)
+	{
+        	ptr = _ptr;
+    	}
+        static void func(const char * match, const unsigned int length, const MatchState & ms)
+        {
+                (ptr->*PTR)(match, length, ms);
+        }
+        operator fn_type()
+        {
+                return &func;
+        }
 private:
-    static T*  ptr;
+        static T*  ptr;
 };
 
-template<class T,
-
-void(T::*PTR)(const char *, const unsigned int, const MatchState &), size_t  I>
+template<class T, void(T::*PTR)(const char *, const unsigned int, const MatchState &), size_t  I>
 T* bind_regex_member<T, PTR, I>::ptr = NULL;
 
 class InterpreterClass
